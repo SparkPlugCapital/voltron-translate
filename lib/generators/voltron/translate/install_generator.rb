976e64a1d4eda165baad3e88f6016ed76c634ad9
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 11:07:11 -0600

    Runs voltron:install generator when installed if initializer does not exist

--- lib/generators/voltron/translate/install_generator.rb
+++ lib/generators/voltron/translate/install_generator.rb
@@ -9,13 +9,19 @@ module Voltron
 
 					voltron_initialzer_path = Rails.root.join("config", "initializers", "voltron.rb")
 
-					if File.exist? voltron_initialzer_path
+					unless File.exist? voltron_initialzer_path
+						unless system("cd #{Rails.root.to_s} && rails generate voltron:install")
+							puts "Voltron initializer does not exist. Please ensure you have the 'voltron' gem installed and run `rails g voltron:install` to create it"
+							return false
+						end
+					end
 
-						current_initiailzer = File.read voltron_initialzer_path
+					current_initiailzer = File.read voltron_initialzer_path
 
-						unless current_initiailzer.match(Regexp.new(/^\s# === Voltron Translate Configuration ===\n/))
-							inject_into_file(voltron_initialzer_path, after: "Voltron.setup do |config|\n") do
+					unless current_initiailzer.match(Regexp.new(/^\s# === Voltron Translate Configuration ===\n/))
+						inject_into_file(voltron_initialzer_path, after: "Voltron.setup do |config|\n") do
 <<-CONTENT
+
 	# === Voltron Translate Configuration ===
 
 	# Whether or not translation is enabled
@@ -27,9 +33,7 @@ module Voltron
 	# In what environments can translation generation occur. Recommended to keep this as development (default)
 	# config.translate.build_environment << :development
 CONTENT
-							end
 						end
-						
 					end
 				end
 			end
