cd8632bfa8a1fe580ea16e3feecee8aca392fbf5
Eric Hainer <eric.hainer@gmail.com>
2016-11-01 16:37:48 -0600

    extends ActiveRecord::Base with translate method

--- lib/voltron/translate/railtie.rb
+++ lib/voltron/translate/railtie.rb
@@ -6,6 +6,7 @@ module Voltron
       initializer "voltron.translate.initialize" do
         ::ActionController::Base.send :include, ::Voltron::Translate
         ::ActiveRecord::Base.send :include, ::Voltron::Translate
+        ::ActiveRecord::Base.send :extend, ::Voltron::Translate
         ::ActionView::Base.send :include, ::Voltron::Translate
         ::ActionMailer::Base.send :include, ::Voltron::Translate
       end
